fb147696938712358264c512c0981e8d6c5f57bd
Mehedințu Mircea Vlăduț <36994718+vlad420@users.noreply.github.com>
2024-08-14 22:04:36 +0300

    Incarcare comenzi custom

--- init.lua
+++ init.lua
@@ -93,6 +93,9 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+-- Commands
+require 'custom.commands'
+
 -- [[ Setting options ]]
 require 'options'
 
