2104237a01a848bb4086df1c469265b16bc1bfab
Mehedințu Mircea Vlăduț <36994718+vlad420@users.noreply.github.com>
2024-08-25 13:03:30 +0300

    Setare terminal implicit Powershell in windows

---
+++ lua/custom/options.lua
@@ -0,0 +1,10 @@
+if vim.loop.os_uname().sysname == 'Windows_NT' then
+  vim.o.shell = 'powershell'
+  vim.o.shellcmdflag =
+    '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
+  vim.o.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+  vim.o.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+  vim.o.shellquote = ''
+  vim.o.shellxquote = ''
+end
+
