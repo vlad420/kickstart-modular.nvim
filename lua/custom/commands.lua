1a8a68ef0d13475253cc785f3a7729cfc79bb79e
Mehedințu Mircea Vlăduț <36994718+vlad420@users.noreply.github.com>
2024-08-25 17:13:28 +0300

    verificare OS

--- lua/custom/commands.lua
+++ lua/custom/commands.lua
@@ -1,7 +1,9 @@
-vim.cmd [[
+if vim.loop.os_uname().sysname == 'Windows_NT' then
+  vim.cmd [[
 function! OpenWindowsTerminal()
   let l:project_root = getcwd()
   execute '!wt -d' shellescape(l:project_root)
 endfunction
 command! Wt call OpenWindowsTerminal()
 ]]
+end
