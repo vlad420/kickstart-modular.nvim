e6ea0c76132b8cf5b5c0a0f3214cf3ea0ad9e49c
Mehedințu Mircea Vlăduț <36994718+vlad420@users.noreply.github.com>
2024-10-18 17:32:48 +0300

    Adaugare scurtatura pentru Copilot pentru accept next word

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -4,4 +4,6 @@ vim.keymap.set('n', '<C-s>', '<cmd>write<CR>', { desc = 'Save document' })
 vim.keymap.set('i', '<C-s>', '<cmd>write<CR><cmd>stopinsert<CR>', { desc = 'Save document and exit insert mode' })
 
 -- Accept the next word of the current suggestion
-vim.keymap.set('i', '<C-x>', '<Plug>(copilot-accept-word)')
+vim.keymap.set('i', '<C-Right>', '<Plug>(copilot-accept-word)')
+-- Force a new suggestion
+-- vim.keymap.set('i', '<C-Right>', '<Plug>(copilot-suggest)')
