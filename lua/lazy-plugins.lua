46e230067f610b0bfa5c30047e38820f4555c092
Mehedințu Mircea Vlăduț <36994718+vlad420@users.noreply.github.com>
2025-01-11 21:17:46 +0200

    New theme

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -40,7 +40,8 @@ require('lazy').setup({
 
   require 'kickstart/plugins/cmp',
 
-  require 'kickstart/plugins/tokyonight',
+  require 'kickstart/plugins/kanagawa',
+  -- require 'kickstart/plugins/tokyonight',
 
   require 'kickstart/plugins/todo-comments',
 
